59bca3c86c3920c0ab02d53e6b419ee7ef96e200
João Paulo Ricardo <tryllerot@gmail.com>
2018-07-16 14:43:56 -0300

    amumu update

--- aaaaa funcoes.lua
+++ aaaaa funcoes.lua
@@ -3,6 +3,7 @@ BlockCast()
 
 ts = TargetSelector(myHero:GetSpellData(_Q).range, TARGET_LOW_HP, DAMAGE_MAGIC, true)
 config:TargetSelector("ts", "Target Selector", ts)
+local target = ts:GetTarget()
 
 function Mode()
 	if _G.IOW_Loaded and IOW:Mode() then
@@ -21,13 +22,13 @@ end
 function ResetAA()
     if _G.IOW_Loaded then
         return IOW:ResetAA()
-        elseif _G.PW_Loaded then
+	elseif _G.PW_Loaded then
         return PW:ResetAA()
-        elseif _G.DAC_Loaded then
+	elseif _G.DAC_Loaded then
         return DAC:ResetAA()
-        elseif _G.AutoCarry_Loaded then
-        return DACR:ResetAA()
-        elseif _G.SLW_Loaded then
+	elseif _G.AutoCarry_Loaded then
+		return DACR:ResetAA()
+	elseif _G.SLW_Loaded then
         return SLW:ResetAA()
     end
 end
@@ -35,13 +36,13 @@ end
 function SetAttack(boolean)
 	if _G.IOW_Loaded then
 		return IOW.attacksEnabled == (boolean)
-		elseif _G.PW_Loaded then
+	elseif _G.PW_Loaded then
         return PW.attacksEnabled == (boolean)
-        elseif _G.DAC_Loaded then
+	elseif _G.DAC_Loaded then
         return DAC.attacksEnabled == (boolean)
-        elseif _G.AutoCarry_Loaded then
+	elseif _G.AutoCarry_Loaded then
         return DACR.attacksEnabled == (boolean)
-        elseif _G.SLW_Loaded then
+	elseif _G.SLW_Loaded then
         return SLW.attacksEnabled == (boolean)
     end
 end
--- amumu.lua
+++ amumu.lua
@@ -1,19 +1,18 @@
 if GetObjectName(GetMyHero()) ~= "Amumu" then return end
 
 require ("DamageLib")
+local config = Menu("Amumu", "Amumu")
+config:SubMenu("Combo", "Combo Settings")
+config.Combo:Boolean("Q", "Use Q", true)
+config.Combo:Boolean("W", "Use W", true)
+config.Combo:Boolean("E", "Use E", true)
+config.Combo:Boolean("R", "Use R", true)
+config.Combo:Slider("Mana", "Min. Mana For W", 50, 0, 100, 1)
+config.Combo:Slider("MR", "Min. Enemy For R", 2, 1, 5, 1)
 
--- Menu
-local mainMenu = Menu("Beta's Amumu", "Beta's Amumu")
--- Combo
-mainMenu:SubMenu("Combo", "Combo Settings")
-mainMenu.Combo:Boolean("Q", "Use Q", true)
-mainMenu.Combo:Boolean("W", "Use W", true)
-mainMenu.Combo:Boolean("E", "Use E", true)
-mainMenu.Combo:Boolean("R", "Use R", true)
-mainMenu.Combo:Slider("Mana", "Min. Mana For W", 50, 0, 100, 1)
-mainMenu.Combo:Slider("MR", "Min. Enemy For R", 2, 1, 5, 1)
+ts = TargetSelector(myHero:GetSpellData(_Q).range, TARGET_LOW_HP, DAMAGE_MAGIC, true)
+config:TargetSelector("ts", "Target Selector", ts)
 
--- Orbwalker's
 function Mode()
 	if _G.IOW_Loaded and IOW:Mode() then
 		return IOW:Mode()
@@ -28,34 +27,28 @@ function Mode()
 	end
 end
 
--- Tick
 OnTick(function()
-
-	local target = GetCurrentTarget()
+	local target = ts:GetTarget()
 	if Mode() == "Combo" then
-		-- Q
-		if mainMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 1100) then
+		if config.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 1100) then
 			local QPred = GetPredictionForPlayer(GetOrigin(myHero), target, GetMoveSpeed(target), 2000, 0.25, 1100, 30, true, true)
 			if QPred.HitChance == 1 then	
 				CastSkillShot(_Q, QPred.PredPos)
 			end
 		end	
-			-- E
-		if mainMenu.Combo.E:Value() and Ready(_E) and ValidTarget(target, 350) then	
+
+		if config.Combo.E:Value() and Ready(_E) and ValidTarget(target, 350) then	
         	CastTargetSpell(target , _E)
 		end
-		
-		-- W
-		if (myHero.mana/myHero.maxMana >= mainMenu.Combo.Mana:Value() /100) then
-		
-				if ValidTarget(target, 300) and Ready(_W)  then
-			CastSpell(_W)
-		        end
+
+		if (myHero.mana/myHero.maxMana >= config.Combo.Mana:Value() /100) then
+			if ValidTarget(target, 300) and Ready(_W)  then
+				CastSpell(_W)
+			end
 		end
-		-- R
-				if Ready(_R) and EnemiesAround(myHero, 560) >= mainMenu.Combo.MR:Value() and mainMenu.Combo.R:Value() then
+
+		if Ready(_R) and EnemiesAround(myHero, 550) >= config.Combo.MR:Value() and config.Combo.R:Value() then
 			CastSpell(_R)
-		        end
-		
-    end
+		end
+	end
 end)
