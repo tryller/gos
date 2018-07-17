b2d6e0137b33fba1fded0b8df4869d62715ecd5f
João Paulo Ricardo <tryllerot@gmail.com>
2018-07-17 12:34:06 -0300

    update dr mundo

--- dr.mundo.lua
+++ dr.mundo.lua
@@ -1,13 +1,17 @@
 if GetObjectName(myHero) ~= "DrMundo" then return end
--- Keys
-mainMenu = Menu("DrMundo", "DrMundo")
--- Combo menu
-mainMenu:SubMenu("Combo", "Combo")
-mainMenu.Combo:Boolean("Q", "Use Q", true)
-mainMenu.Combo:Boolean("W", "Use W", true)
-mainMenu.Combo:Boolean("E", "Use E", true)
 
--- Orbwalker's
+local config = Menu("DrMundo", "DrMundo")
+config:SubMenu("Combo", "Combo")
+config.Combo:Boolean("Q", "Use Q", true)
+config.Combo:Boolean("W", "Use W", true)
+config.Combo:Boolean("E", "Use E", true)
+
+ts = TargetSelector(myHero:GetSpellData(_Q).range, TARGET_LOW_HP, DAMAGE_MAGIC, true)
+ts = TargetSelector(myHero:GetSpellData(_W).range, TARGET_LOW_HP, DAMAGE_PHYSICAL, true)
+config:TargetSelector("ts", "Target Selector", ts)
+
+local WActive = false
+
 function Mode()
 	if _G.IOW_Loaded and IOW:Mode() then
 		return IOW:Mode()
@@ -22,22 +26,44 @@ function Mode()
 	end
 end
 
-OnTick(function(myHero)
-	if Mode() == "Combo" then
-local unit = GetCurrentTarget()
-if ValidTarget(unit, 1300) then
-
--- Dr.Mundo Q
-	if mainMenu.Combo.Q:Value() then
-        	CastSkillShot(_Q, unit.x, unit.y, unit.z)
+function IsEnemyInRange(enemy, range)
+	local result = 0
+	if ValidTarget(enemy, range) then
+		result = result + 1
 	end
 
-
--- Dr.Mundo E
-if mainMenu.Combo.E:Value() then
-	CastSpell(_E)
+	return result
 end
 
-end
-end
+OnTick(function(myHero)
+	local target = ts:GetTarget()
+	if Mode() == "Combo" then
+		if config.Combo.Q:Value() and ValidTarget(target, 975) then
+			CastSkillShot(_Q, target.x, target.y, target.z)
+		end
+
+		if not WActive and config.Combo.W:Value() and EnemiesAround(myHeroPos(), 325) >= 1 then
+			CastSpell(_W)
+		else
+			if WActive and EnemiesAround(myHeroPos(), 325) == 0 then
+				CastSpell(_W)
+			end
+		end
+
+		if config.Combo.E:Value() and ValidTarget(target, 300) then
+			CastSpell(_E)
+		end
+	end
+end)
+
+OnUpdateBuff(function(unit,buff)
+	if unit == myHero and buff.Name == "BurningAgony" then 
+		WActive = true
+	end
+end)
+
+OnRemoveBuff(function(unit,buff)
+	if unit == myHero and buff.Name == "BurningAgony" then 
+		WActive = false
+	end
 end)
